53565526c5f0866871568c3af3995e9bbff374a2
Nissy <chihiro_24s1200167@nnn.ed.jp>
2025-05-12 02:27:30 +0900

    Tailwindの設定完了とfooter作成開始

---
+++ src/renderer/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="">
+        <ul class="flex">
+            <li>
+                <router-link to="/">
+                    Home
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/Calendar">
+                    Calender
+                </router-link>
+            </li>
+        </ul>
+    </footer>
+</template>
+
--- src/renderer/src/router.js
+++ src/renderer/src/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 //ページ読み込み
 import Home from './views/Home.vue'
+import Calendar from "./views/Calendar.vue";
 
 //リンク一覧
 const routes = [
     { path: '/', component: Home },
+    { path: '/Calendar', component: Calendar },
 ]
 
 const router = createRouter({
